db7c22b0a957deb537cb751b27717c969dd1c8f1
usmanov <ussarvar@gmail.com>
2022-01-17 21:13:01 +0500

    Adding student scores Secret Auction project files

--- projects/Secret_Auction/main.py
+++ projects/Secret_Auction/main.py
@@ -1,24 +1,47 @@
-programming_dictionary = {
-    "Bug": "An error in a program that prevents the program from running as expected.", 
-    "Function": "A piece of code that you can easily call over and over again."
+# programming_dictionary = {
+#     "Bug": "An error in a program that prevents the program from running as expected.", 
+#     "Function": "A piece of code that you can easily call over and over again."
+# }
+# # Retrieving items from the dictionary 
+# print(programming_dictionary["Bug"])
+# #  adding new items to the dictionary
+# programming_dictionary["Loop"] = "The action of doing the something again and again"
+
+# # Creating an empty dictionary
+# # empty_dictionary = {}
+
+# # Wipe an existing dictionary 
+# # programming_dictionary = {}
+
+# # Edit an item in dictionary 
+# programming_dictionary["Bug"] = "A mug inside your computer."
+
+# # print(programming_dictionary)
+
+# # Loop through Dictionary 
+# for key in programming_dictionary:
+#     print(key)
+#     print(programming_dictionary[key])
+
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
 }
-# Retrieving items from the dictionary 
-print(programming_dictionary["Bug"])
-#  adding new items to the dictionary
-programming_dictionary["Loop"] = "The action of doing the something again and again"
+# 🚨 Don't change the code above 👆
 
-# Creating an empty dictionary
-# empty_dictionary = {}
+#TODO-1: Create an empty dictionary called student_grades.
 
-# Wipe an existing dictionary 
-# programming_dictionary = {}
+student_grades = {}
+#TODO-2: Write your code below to add the grades to student_grades.👇
 
-# Edit an item in dictionary 
-programming_dictionary["Bug"] = "A mug inside your computer."
 
-# print(programming_dictionary)
 
-# Loop through Dictionary 
-for key in programming_dictionary:
-    print(key)
-    print(programming_dictionary[key])
+# Expected Output
+# '{'Harry': 'Exceeds Expectations', 'Ron': 'Acceptable', 'Hermione': 'Outstanding', 'Draco': 'Acceptable', 'Neville': 'Fail'}'
+    
+
+# 🚨 Don't change the code below 👇
+print(student_grades)
