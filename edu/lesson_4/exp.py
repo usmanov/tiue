0ad80ffc75792b30083365811656d8155c9f47e3
usmanov <ussarvar@gmail.com>
2022-02-15 14:51:15 +0500

    Commiting with Pycharm

--- edu/lesson_4/exp.py
+++ edu/lesson_4/exp.py
@@ -519,13 +519,13 @@
 
 # a1 = B()
 # a1.show() # --> in A show
-
+#
 # class A:
 #     def show(self):
 #         print("in A show")
 # class B:
 #     def show(self):
 #         print("in B show")
-
+#
 # a1 = B()
 # a1.show() # --> in B show
